c2e60893ba1513076691ff36a214de13a4c12ed3
hasanravshanovv <ravshanovhasana@gmail.com>
2024-09-10 18:49:21 +0300

    finish back almost

--- next-pizza-study/my-app/src/components/shared/header.tsx
+++ next-pizza-study/my-app/src/components/shared/header.tsx
@@ -5,6 +5,8 @@ import { Container } from "./container"
 import Image from "next/image"
 import { Button } from "../ui"
 import { ArrowRight, ShoppingCart, User } from "lucide-react"
+import Link from "next/link"
+import { SearchInput } from "./search-input"
 
 interface Props {
   className?: string
@@ -16,15 +18,21 @@ export const Header: React.FC<Props> = ({ className }) => {
       <Container className="flex items-center justify-between py-8">
         {/* left site */}
 
-        <div className="flex items-center gap-4">
-          <Image src="/logo.png" alt="header_logo" width={35} height={35} />
-          <div>
-            <h1 className="text-2xl uppercase font-black"> Next Pizza</h1>
+        <Link href="/">
+          <div className="flex items-center gap-4">
+            <Image src="/logo.png" alt="header_logo" width={35} height={35} />
+            <div>
+              <h1 className="text-2xl uppercase font-black"> Next Pizza</h1>
 
-            <p className="text-sm text-gray-400 leading-3">
-              вкусней уже некуда
-            </p>
+              <p className="text-sm text-gray-400 leading-3">
+                вкусней уже некуда
+              </p>
+            </div>
           </div>
+        </Link>
+
+        <div className="mx-10 flex-1">
+          <SearchInput className="" />
         </div>
 
         {/* right site */}
